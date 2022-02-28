bc569fdf64fe7ed73fb65a4e72b6ccf26506d695
svc-developer <svc.developer@avalara.com>
2022-02-28 17:57:18 +0000

    2.4.27

--- src/main/java/Avalara/SDK/Pair.java
+++ src/main/java/Avalara/SDK/Pair.java
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * Avalara Shipping Verification for Beverage Alcohol
+ * Avalara Shipping Verification only
  *
  * API for evaluating transactions against direct-to-consumer Beverage Alcohol shipping regulations.  This API is currently in beta. 
  *
@@ -14,7 +14,7 @@
  * @author     Jonathan Wenger <jonathan.wenger@avalara.com>
  * @copyright  2004-2022 Avalara, Inc.
  * @license    https://www.apache.org/licenses/LICENSE-2.0
- * @version    2.4.22
+ * @version    2.4.27
  * @link       https://github.com/avadev/AvaTax-REST-V3-JRE-SDK
  */
 
